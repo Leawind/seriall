ee44cbf940f9e354cb4f14fdf59e7b4b47f935f7
Leawind <leawind@yeah.net>
2025-01-25 19:20:18 +0800

    add npm example

---
+++ examples/npm/index.js
@@ -0,0 +1,16 @@
+import * as seriall from "@leawind/seriall";
+
+class A {
+	id;
+	constructor(id) {
+		this.id = id;
+	}
+}
+
+const context = { palette: { A } };
+
+const a = new A(1234);
+
+const str = seriall.stringify(a, context);
+
+console.log(str);
