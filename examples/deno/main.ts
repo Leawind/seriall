758003b94a6f947de50b489a2f7957121c916d18
Leawind <leawind@yeah.net>
2025-01-25 17:54:07 +0800

    add deno example

---
+++ examples/deno/main.ts
@@ -0,0 +1,16 @@
+import * as seriall from "@leawind/seriall";
+
+class A {
+  private id: number;
+  public constructor(id: number) {
+    this.id = id;
+  }
+}
+
+const context: seriall.ContextLike = { palette: { A } };
+
+const a = new A(1234);
+
+const str = seriall.stringify(a, context);
+
+console.log(str);
